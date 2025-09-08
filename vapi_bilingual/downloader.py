514520099ac89b80ed5e4b8bc07ff1c34e0ffeff
masteliclovre <lovre.mastelic@fer.unizg.hr>
2025-09-08 14:36:32 +0200

    brisanje
    nepotrebno vise

--- vapi_bilingual/downloader.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import snapshot_download
-# replace with the exact repo id you’re using:
-repo_id = "Gorans/whisper-large-v3-turbo-hr-parla-ctranslate2"
-local_dir = snapshot_download(repo_id=repo_id, local_dir="whisper-large-v3-turbo-hr-parla-ctranslate2", local_dir_use_symlinks=False)
-print("Downloaded to:", local_dir)
